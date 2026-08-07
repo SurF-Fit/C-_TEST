9307d8d51f5f44a4aa591dd8845a8564c580adc3
SurF-Fit <kkulmenev@gmail.com>
2026-08-07 12:35:27 +0700

    Adding an intermediate architecture

---
+++ back/TimescaleDataProcessor.Core/Exceptions/ValidationException.cs
@@ -0,0 +1,10 @@
+namespace TimescaleDataProcessor.Core.Exception
+{
+    public class ValidationEception : Exception
+    {
+        public ValidationEception (string message) : base(message)
+        {
+            
+        }
+    }    
+}
